0f7205c6bce4039cfb20847bf7ed3a3b5567b615
Jose <jblanco@interactions.com>
2019-03-24 08:03:41 -0400

    port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 8080;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
