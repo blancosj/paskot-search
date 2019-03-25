d3a71a4d705ac165b9f341365622204de7719630
Jose <jblanco@interactions.com>
2019-03-24 20:58:16 -0400

    web

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -12,9 +12,9 @@ module.exports = {
     __dirname: false,   // if you don't put this is, __dirname
     __filename: false,  // and __filename return blank or /
   },
-  externals: [nodeExternals()], // Need this to avoid error when working with Express    
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
   output: {
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, 'dist'),
     // publicPath: 'dist',
     filename: 'bundle.js',
   }
