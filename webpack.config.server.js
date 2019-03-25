eef4a462ed41097a5e6d8947f3ace9a7e7e3a50d
Jose <jblanco@interactions.com>
2019-03-24 20:54:19 -0400

    web

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,10 +1,21 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false,   // if you don't put this is, __dirname
+    __filename: false,  // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express    
   output: {
     path: path.resolve(__dirname, "../dist"),
     // publicPath: 'dist',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   }
 }
