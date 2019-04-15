0a9de436a705656e8278809278bfe64231a1ce4b
Jose <jblanco@interactions.com>
2019-04-15 00:46:08 -0400

    added adsense

--- frontend/src/AdsItem.jsx
+++ frontend/src/AdsItem.jsx
@@ -11,6 +11,7 @@ class AdsItem extends React.Component {
       <section class="results">
         <ins class="adsbygoogle"
              style={{display:"block"}}
+             data-ad-test="adtest"
              data-ad-format="fluid"
              data-ad-layout-key="-6p+dg+59-2c-8d"
              data-ad-client="ca-pub-7090864477039814"
